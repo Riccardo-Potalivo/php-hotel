4ac40b282d67e24f9bdea83b2d9c775037fdab04
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-29 16:11:26 +0100

    milestone-2-complete

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include __DIR__ . '/partials/header.php';
 
 ?>
 
-<main class="container">
+<main class="container py-5">
     <table class="table">
         <thead>
             <tr>
@@ -25,13 +25,22 @@ include __DIR__ . '/partials/header.php';
                         <?php echo $hotel['description'] ?>
                     </td>
                     <td>
-                        <?php echo $hotel['parking'] ?>
+                        <?php echo $hotel['parking'] ?
+                            '<i class="fa-solid fa-check text-success "></i>' :
+                            '<i class="fa-solid fa-xmark text-danger "></i>'
+                            ?>
+
                     </td>
                     <td>
-                        <?php echo $hotel['vote'] ?>
+                        <?php for ($i = 1; $i <= 5; $i++) { ?>
+                            <?php echo $hotel['vote'] >= $i ?
+                                '<i class="fa-solid fa-star"></i>' :
+                                '<i class="fa-regular fa-star"></i>'
+                                ?>
+                        <?php } ?>
                     </td>
                     <td>
-                        <?php echo $hotel['distance_to_center'] ?>
+                        <?php echo $hotel['distance_to_center'] ?> Km
                     </td>
                 </tr>
             <?php } ?>
