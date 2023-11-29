a9588a27e64fadee2d72dc2c400be5a7a49597e6
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-11-29 15:25:32 +0100

    milestone-1-complete

--- index.php
+++ index.php
@@ -5,6 +5,16 @@ include __DIR__ . '/partials/header.php';
 ?>
 
 <main class="container">
+    <?php foreach ($hotels as $hotel) { ?>
+        <div>
+            <h2>
+                <?php echo $hotel['name'] ?>
+            </h2>
+            <p>
+                <?php echo $hotel['description'] ?>
+            </p>
+        </div>
+    <?php } ?>
 
 </main>
 
